31d379bb4d4712404764578403916b68a2ccb2ca
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-23 13:48:08 -0600

    trying to get the <p> to show how many times the button was pushed

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(onReady);
 
+let generateCount = 0;
+
 function onReady() {
     console.log('in on ready!');
 
@@ -10,6 +12,8 @@ function onReady() {
 function addDiv() {
     console.log('in add div');
     $("body").append(`<div>`);
+    <p>${generateCount}++ I want this to show how many times generate button was pushed</p>
+
     
 }
 
