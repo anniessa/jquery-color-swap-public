11e1fffe9e85c8fe3fee402fe8aba14224ed7720
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-23 14:26:46 -0600

    still unsure how to get yellow background to appear when clicking yellow button and also the rendering of generate button number is still weird

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 $(document).ready(onReady);
 
-let generateCount = 1;
+let generateCount = 0;
 
 function onReady() {
     console.log('in on ready!');
 
-    $("body").on('click', ".generateBtn", render)
+    $("body").on('click', ".generateBtn", calculateGenerate)
     $(document).on('click', ".yellowBtn", changeYellow)
     $(document).on('click', ".deleteBtn", deleteDiv)
-
-        
     }
+
+function calculateGenerate() {
+    console.log('in calculate generate');
+    generateCount++
+    render();
+}
     
 function changeYellow() {
     console.log('in change yellow:');
-    $(this).css("backgroundColor", "yellow");
-    
+    $("div").addClass('.yellowBackground');
 }
 
 function deleteDiv() {
     console.log('in delete div');
-    $(this).closest('div').remove();
-    
+    $(this).closest('div').remove(); 
 }
     
 function render() {
     console.log('in add div');
     $("body").append(`<div>`);
     $("div").append(`
-    <p>${generateCount++}</p>
+    <p>${generateCount}</p>
     <button class="yellowBtn">Yellow</button>
     <button class="deleteBtn">Delete</button>
     `)
