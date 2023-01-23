8a2b285d8e6c2e353e8edc1de830b8929bee9216
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-23 13:28:32 -0600

    updated ready function because I wasn't seeing my onReady function

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-$(document).on(onReady);
+$(document).ready(onReady);
 
 function onReady() {
     console.log('in on ready!');
+
+    $("#generateBtn").on('click', )
     
 }
 
 function render() {
+    $("generateBtn").on('click').append("div")
     
 }
