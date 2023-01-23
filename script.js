91eae1c912a6d2e2afb0b5de49f09a4e879f0f54
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-23 13:37:24 -0600

    not getting the button to interact with jquery

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ $(document).ready(onReady);
 function onReady() {
     console.log('in on ready!');
 
-    $(document).on('click', "#generateBtn",)
+    $("body").on('click', "#generateBtn", buttonDiv)
+        
+    }
     
-}
 
 function buttonDiv() {
-
+    console.log('in button div');
+    $("body").append("div")
+    render();
 }
 
 function render() {
-    $("generateBtn").on('click').append("div")
+    $("body").append("div")
+
     
 }
