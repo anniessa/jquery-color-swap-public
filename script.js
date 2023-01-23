06c810220f2ea3698c78569f56b3e2dec6811b9b
Anniessa Antar <anniessa.antar@gmail.com>
2023-01-23 14:11:29 -0600

    added yellow button and delete button, yellow button just changes the button, not the associated divs and delete button still not working

--- script.js
+++ script.js
@@ -5,17 +5,31 @@ let generateCount = 1;
 function onReady() {
     console.log('in on ready!');
 
-    $("body").on('click', ".generateBtn", addDiv)
+    $("body").on('click', ".generateBtn", render)
+    $(document).on('click', ".yellowBtn", changeYellow)
+    $(document).on('click', ".deleteBtn", deleteDiv)
+
         
     }
     
-function addDiv() {
+function changeYellow() {
+    $(this).css("backgroundColor", "yellow");
+    
+}
+
+function deleteDiv() {
+    console.log('in delete div');
+    $(this).closest('div');
+    
+}
+    
+function render() {
     console.log('in add div');
     $("body").append(`<div>`);
     $("div").append(`
     <p>${generateCount++}</p>
-    <button>Yellow</button>
-    <button>Delete</button>
+    <button class="yellowBtn">Yellow</button>
+    <button class="deleteBtn">Delete</button>
     `)
     
 }
